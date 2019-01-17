06fdecdb36241db81de0952eccfb940aaf129dfb
DexterSnyder <40254136+DexterSnyder@users.noreply.github.com>
2019-01-16 18:30:31 -0700

    Wed Jan 16 18:30:30 2019

--- ACES_Cpp/ACES_Cpp/GitInterface.cpp
+++ ACES_Cpp/ACES_Cpp/GitInterface.cpp
@@ -1,3 +1,4 @@
+//Visual Studio flags ctime as unsafe. Need to suppress the warnings
 #define _CRT_SECURE_NO_WARNINGS 0
 
 #include <stdlib.h>
@@ -7,7 +8,7 @@
 
 void gitCommit()
 {
-	//get the time in string form
+	//get the time in string form - Credit : http://www.cplusplus.com/reference/ctime/localtime/
 	time_t rawtime;
 	struct tm * timeinfo;
 
@@ -18,12 +19,16 @@ void gitCommit()
 	//now we have to trim the /n off the end of the string
 	tempTime = tempTime.substr(0, tempTime.size() - 1);
 
+	//build the command to commit to the local branch
 	std::string commitCommand = "git commit -m \"" +tempTime;
 	commitCommand += "\"";
+
 	//run commmands on the cmd line
 	system("git add -A");
-	//system("git commit -m \"" + my_time + "\"");
 	system(commitCommand.c_str());
+
+	//clean up
+	delete timeinfo;
 }
 
 
