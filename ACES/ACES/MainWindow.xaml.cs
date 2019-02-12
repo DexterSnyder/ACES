9430044733ea3221b2e11535d3aba4bc1634dd04
JaredMorales <jaredmorales@mail.weber.edu>
2019-02-11 18:19:48 -0700

    GUI Fixed

--- ACES/ACES/MainWindow.xaml.cs
+++ ACES/ACES/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ACES_GUI.CreateClass;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -14,14 +15,13 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ACES
+namespace ACES_GUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-
         ObservableCollection<Class> classList = new ObservableCollection<Class>();
 
         public MainWindow()
@@ -29,7 +29,31 @@ namespace ACES
             InitializeComponent();
             GetClassList();
             SaveClassList();
-            //dropDown name: .ItemsSource  = classList; 
+            RosterDataGrid();
+        }
+
+        private void RosterDataGrid()
+        {
+            DataGridTextColumn col1 = new DataGridTextColumn();
+            DataGridTextColumn col2 = new DataGridTextColumn();
+            DataGridTextColumn col3 = new DataGridTextColumn();
+            DataGridTextColumn col4 = new DataGridTextColumn();
+            studentFilesList.Columns.Add(col1);
+            studentFilesList.Columns.Add(col2);
+            studentFilesList.Columns.Add(col3);
+            studentFilesList.Columns.Add(col4);
+            col1.Binding = new Binding("Name");
+            col2.Binding = new Binding("Score");
+            col3.Binding = new Binding("Rating");
+            col4.Binding = new Binding("Result");
+            col1.Width = 78.5;
+            col2.Width = 78.5;
+            col3.Width = 78.5;
+            col4.Width = 78.5;
+            col1.Header = "Name";
+            col2.Header = "Score";
+            col3.Header = "Rating";
+            col4.Header = "Result";
         }
 
         private void SaveClassList()
@@ -40,6 +64,12 @@ namespace ACES
         private void GetClassList()
         {
             throw new NotImplementedException();
+        }
+
+        private void CreateClass_Click(object sender, RoutedEventArgs e)
+        {
+            var createWindow = new CreateClass.CreateClass();
+            createWindow.ShowDialog();
         }
     }
 }
