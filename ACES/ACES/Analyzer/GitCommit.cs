6f2aa74d16b5a480688c35712f779539e4e54244
DexterSnyder <40254136+DexterSnyder@users.noreply.github.com>
2019-02-20 19:01:15 -0700

    Working on GitCommit class

--- ACES/ACES/Analyzer/GitCommit.cs
+++ ACES/ACES/Analyzer/GitCommit.cs
@@ -23,6 +23,9 @@ namespace ACES
         /// </summary>
         public string Author { get; private set; }
 
+        /// <summary>
+        /// public constructor
+        /// </summary>
         public GitCommit()
         {
             CommitDateTime = new DateTime();
@@ -30,14 +33,39 @@ namespace ACES
             Author = "";
         }
 
-        public void PopulateDataFields(string[] input)
-        {
-
-        }
-
+        /// <summary>
+        /// Populates the data fields
+        /// </summary>
+        /// <param name="date">The date of the commit</param>
+        /// <param name="message">The commit message</param>
+        /// <param name="author">the commit author</param>
         public void PopulateDataFields (string date, string message, string author)
         {
+            //format = Author: DexterSnyder <40254136+DexterSnyder@users.noreply.github.com>
+            //split on ':' first
+            string[] authorSplit1 = author.Split(':');
+            string authorTemp = authorSplit1[1];
+            //now split on the '<'
+            string[] authorSplit2 = authorTemp.Split('<');
+            author = authorSplit2[0]; //Should contain just the author name
+            //trim leading and trailing whitespace
+            author = author.Trim();
 
+            //format = Date:   Wed Feb 6 17:36:38 2019 -0700
+            //split on ':' first
+            string[] dateSplit1 = date.Split(':');
+            string dateTemp = dateSplit1[1];
+            //Now split on the '-'
+            string[] dateSplit2 = dateTemp.Split('-');
+            date = dateSplit2[0];
+            //trim the leading and trailing whitespace
+            date = date.Trim();
+
+            //format:     Wed Feb  6 17:36:24 2019
+            //Just trim the white spaces for now
+            message = message.Trim();
+
+            //now convert message and date to a date time object
         }
     }
 }
