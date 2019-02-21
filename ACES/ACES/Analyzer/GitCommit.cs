fe26cc17e3f3df4a05e2960fefcb31f0dbcb0c1b
DexterSnyder <40254136+DexterSnyder@users.noreply.github.com>
2019-02-20 18:42:21 -0700

    Added commit data structures

--- ACES/ACES/Analyzer/GitCommit.cs
+++ ACES/ACES/Analyzer/GitCommit.cs
@@ -9,11 +9,35 @@ namespace ACES
     class GitCommit
     {
         /// <summary>
-        /// The date and time of the commit
+        /// The date and time of the commit per git
         /// </summary>
-        public DateTime CommitDateTime {get;set;}
+        public DateTime CommitDateTime {get; private set;}
 
+        /// <summary>
+        /// The date and time contained in the commit message
+        /// </summary>
+        public DateTime CommitMessageDateTime { get; private set; }
 
+        /// <summary>
+        /// Author of the git commit
+        /// </summary>
+        public string Author { get; private set; }
 
+        public GitCommit()
+        {
+            CommitDateTime = new DateTime();
+            CommitMessageDateTime = new DateTime();
+            Author = "";
+        }
+
+        public void PopulateDataFields(string[] input)
+        {
+
+        }
+
+        public void PopulateDataFields (string date, string message, string author)
+        {
+
+        }
     }
 }
