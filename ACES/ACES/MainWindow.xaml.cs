14d557fb89e256a8c9c94db70319bad7ee40be28
JaredMorales <jaredmorales@mail.weber.edu>
2019-03-11 18:26:17 -0600

    Fixed MainWindow
    Made MainWindow bigger and commented out DataGrid columns.

--- ACES/ACES/MainWindow.xaml.cs
+++ ACES/ACES/MainWindow.xaml.cs
@@ -40,6 +40,7 @@ namespace ACES_GUI
         //Datagrid for displaying students assignment scores, info, etc.
         private void RosterDataGrid()
         {
+            /*
             DataGridTextColumn col1 = new DataGridTextColumn();
             DataGridTextColumn col2 = new DataGridTextColumn();
             DataGridTextColumn col3 = new DataGridTextColumn();
@@ -70,7 +71,7 @@ namespace ACES_GUI
             col4.Header = "Avg Commit Time";
             col5.Header = "Std Dev";
             col6.Header = "Compiler Used";
-
+            */
         }
 
         private void SaveClassList()
